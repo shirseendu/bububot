d38aa000beb02034a2fab63acf1846b25d3fb759
shirseendu <shirseendu3@gmail.com>
2017-03-31 14:40:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ var http = require('http')
   res.write('<!doctype html>\n<html lang="en">\n' + 
     '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' + 
     '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' + 
-    '\n\n<h1>By skypebot</h1>\n' +
-
+    '\n\n<h1>MY SKYPE</h1>\n' +
+     
     '\n\n<h1><a href="https://join.skype.com/bot/9862d53f-fe24-4359-9f42-9f6145b7a0f6"><img src="https://dev.botframework.com/Client/Images/Add-To-Skype-Buttons.png"/></a></h1>\n' + 
-    '<div id="content">
-    <p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul>'+
-    '</div>' + 
+    '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' + 
     '\n\n');
   res.end();
  }).listen(port);
