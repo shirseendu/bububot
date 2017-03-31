8877e3ea93901212f3fdfed24a493c3655a818aa
shirseendu <shirseendu3@gmail.com>
2017-03-31 12:48:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var http = require('http')
  var port = process.env.PORT || 1337;
  http.createServer(function(req, res) {
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   response.write("<html><body>NODE </body></html>")
-   res.end('Hello World\n');
+   res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.write('<!doctype html>\n<html lang="en">\n' + 
+    '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' + 
+    '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' + 
+    '\n\n<h1>Euro 2012 teams</h1>\n' + 
+    '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' + 
+    '\n\n');
+  res.end();
  }).listen(port);
