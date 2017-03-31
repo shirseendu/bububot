c745561780b497edf2b827c871aa1e9f3332408a
shirseendu <shirseendu3@gmail.com>
2017-03-31 13:06:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var http = require('http')
     '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' + 
     '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' + 
     '\n\n<h1>Euro 2012 teams</h1>\n' + 
-    '\n\n<h1><a href='+'https://join.skype.com/bot/9862d53f-fe24-4359-9f42-9f6145b7a0f6'>+
-'<img src='+'https://dev.botframework.com/Client/Images/Add-To-Skype-Buttons.png'+'/></a></h1>\n' + 
+    '\n\n<h1><a href="https://join.skype.com/bot/9862d53f-fe24-4359-9f42-9f6145b7a0f6"><img src="https://dev.botframework.com/Client/Images/Add-To-Skype-Buttons.png"/></a></h1>\n' + 
     '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' + 
     '\n\n');
   res.end();
